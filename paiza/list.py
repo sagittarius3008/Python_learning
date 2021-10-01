457858ed7c02325e137ef397eabe5e18a039cc76
sagittarius3008 <sagittarius30008@gmail.com>
2021-10-01 13:16:24 +0000

    paiza

---
+++ paiza/list.py
@@ -0,0 +1,56 @@
+# team = ["勇者", "魔法使い", 100]
+# print(team)
+
+# list = ["戦士","侍","僧侶","魔法使い"]
+# print(list)
+
+# item = ["ロングソード", "ブレードソード", "エクスカリバー"]
+# print(item)
+# print(item[0])
+# num = 0
+# print(item[num + 1])
+
+# len関数
+# print(len(item))
+
+# item.append("蜻蛉切")
+# print(len(item))
+# item[3] = "蜻蛉切改"
+
+# リストの要素の削除
+# item.pop(2)
+
+# for文との組み合わせ
+# for i in item:
+    # print(i)
+
+# print("<select name = 'weapon'>")
+# for weapon in item:
+#     print("<option>" + weapon + "</option>")
+# print("</select>")
+
+# line = input().rstrip().split(",")
+# print(line)
+# print(len(line()))
+
+# str = "One cold rainy day when my father was a little boy he met an old alley cat on his street"
+# word = str.split(" ")
+# print(len(word))
+
+# 複数行データのinput/行数が分からない場合
+# import sys
+# array = []
+# for line in sys.stdin.readlines(): //stdin = standartd input(=キーボードからの入力受付)
+#     array.append(line.rstrip())
+
+# print(array)
+
+
+
+# import random
+# line = input().rstrip()
+# janken = line.split(",")
+# num = len(janken)
+# print(janken)
+# print(janken[random.randrange(num)])
+
---
+++ paiza/loop.py
@@ -0,0 +1,10 @@
+
+# for seireki in range(1926,1989):
+#     syowa = int(seireki) -1925
+#     print("西暦" + str(seireki) + "年は昭和" + str(syowa) + "年です")
+#     seireki += 1
+
+# seireki = input()
+# year = input()
+# for seireki in range(int(seireki), int(seireki) + int(year)):
+#     print("西暦" + str(seireki) +"年は昭和" + str(int(seireki) - 1925) +"年です")
